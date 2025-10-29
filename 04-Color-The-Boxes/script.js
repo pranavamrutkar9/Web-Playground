c6eede3ccb46aede50f1e24b92f2faf8d03c3335
Pranav Amrutkar <pranavamrutkar09@gmail.com>
2025-10-30 00:36:00 +0530

    Delete 04-Color-The-Boxes directory

--- 04-Color-The-Boxes/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".box").forEach(e=>{
-    r = Math.floor((Math.random())*256);
-    g = Math.floor((Math.random())*256);
-    b = Math.floor((Math.random())*256);
-    e.style.backgroundColor= `rgb(${r},${g},${b})`;
-})
